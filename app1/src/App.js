6786dd53e9fb25a66939431c621f9ddeec31ace2
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-29 13:47:46 -0600

    fixed app 1

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constuctor(props) {
-
+  constructor() {
+    super()
     this.state = {
       myName: 'Joe'
     }
